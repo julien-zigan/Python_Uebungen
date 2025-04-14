781be19d77355ab843b364e1119dd34b3f2c6859
Julien Zigan <zigan.julien@gmail.com>
2025-04-14 17:22:00 +0200

    refactor: 010 in OOP

--- .venv/Excercises/010-100/010_Zwei_Zahlen_genau_vergleichen.py
+++ .venv/Excercises/010-100/010_Zwei_Zahlen_genau_vergleichen.py
@@ -7,15 +7,22 @@
 
 from random import randint
 
-def max_of(a, b) -> int:
-    return [[-1, 1], [0]][a==b][a>b]
+class Comparator():
+    def __init__(self):
+        self.LT = "kleiner als"
+        self.GT = "größer als"
+        self.EQ = "genause groß wie"
 
-def max_of_randints(min :int, max :int) -> None:
-    a, b = randint(min, max), randint(min, max)
-    res = [["genauso groß wie"],["kleiner als", "größer als"]]
-    print(a, " ist ", res[max_of(a, b)][max_of(a, b) > 0], b)
+    def max_of(self, a, b) -> int:
+        return [[-1, 1], [0]][a==b][a>b]
+
+    def max_of_randints(self, min :int, max :int) -> None:
+        a, b = randint(min, max), randint(min, max)
+        res = [[self.EQ],[self.LT, self.GT]]
+        print(a, " ist ", res[self.max_of(a, b)][self.max_of(a, b) > 0], b)
 
 
 if __name__ == "__main__":
     for i in range(10):
-        max_of_randints(1, 5)
+        comp = Comparator()
+        comp.max_of_randints(1, 5)
