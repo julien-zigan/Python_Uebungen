440b0888b02a3ff4f3ca9f6d4094e0c4f77fd190
Julien Zigan <zigan.julien@gmail.com>
2025-04-15 03:10:36 +0200

    add 070

--- M070_Fischers Fritze.py
+++ M070_Fischers Fritze.py
@@ -10,3 +10,9 @@ satz = 'Fischers Fritz fischt frische Fische'
 # 3. sche (mit möglichst wenig Zeichen)
 # 4. eci hsr hsfziFseci
 # 5. ii i
+
+print(satz[1:24:3])
+print(satz[7:-1:4])
+print(satz[-4:])
+print(satz[-1::-2])
+print(satz[1::10])
